e865511bd40c4d125a96d093feb30cb33fb143a6
sjm84 <sjm84@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2010-04-20 03:51:41 +0000

    The logger now creates the log file if it does not exist
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@21925 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- src/java/org/greenstone/admin/LoggedMessageArea.java
+++ src/java/org/greenstone/admin/LoggedMessageArea.java
@@ -15,6 +15,21 @@ public class LoggedMessageArea extends JTextArea
     public LoggedMessageArea(Class loggerClass)
     {
 	if(!_loggingLoaded){
+	    try{
+		File logPath = new File(".." + File.separatorChar + "ext" + File.separatorChar + "logs");
+		File logFile = new File(".." + File.separatorChar + "ext" + File.separatorChar + "logs" + File.separatorChar + "ext.log");
+	
+		if(!logPath.exists()){
+		    logPath.mkdirs();
+		}
+
+		if(!logFile.exists()){
+		    logFile.createNewFile();
+		}
+	    }
+	    catch(Exception ex){
+		ex.printStackTrace();
+	    }
 	    PropertyConfigurator.configure("log4j.properties");
 	    _loggingLoaded = true;
 	}
