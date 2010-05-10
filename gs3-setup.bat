e2ad99d0ebc8b21872dd7c644734e4e3bd8ef101
ak19 <ak19@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2010-05-10 05:58:31 +0000

    1. Fixed bug that Dr Nichols found where RUNJAVA wasn't been passed on to gs3-server.bat. Needed to redo the set operation on RUNJAVA in combination with the endlocal command, otherwise it didn't seem to work (for other variables this was not necessary). 2. Moved set enabledelayedexpansion to the top of the file so that any other uses of filepaths in variables could be referred to with the recent changeover to exclamation marks instead of percent signs (for dealing with whitespaces in filepaths).
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@22069 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- gs3-setup.bat
+++ gs3-setup.bat
@@ -1,5 +1,7 @@
 @echo off
 
+setlocal enabledelayedexpansion
+
 set java_min_version=1.5.0_00
 
 if exist gs3-setup.bat goto start
@@ -10,44 +12,44 @@ if exist gs3-setup.bat goto start
 :: ---- Set some relevant environment variables ----
 
 :: this is the default
-:: greenstone3 (%GSDL3SRCHOME%)
-:: +-- web (%GSDL3HOME%)
+:: greenstone3 (!GSDL3SRCHOME!)
+:: +-- web (!GSDL3HOME!)
 :: +-- packages
-::      +-- tomcat (%TOMCAT_HOME%)
-::      +-- ant (%ANT_HOME%)
+::      +-- tomcat (!TOMCAT_HOME!)
+::      +-- ant (!ANT_HOME!)
 
 :: set GSDLSRCHOME to the current directory
-cd | winutil\setvar.exe GSDL3SRCHOME > %TMP%\setgsdl3.bat
-call %TMP%\setgsdl3.bat
-del %TMP%\setgsdl3.bat
+cd | winutil\setvar.exe GSDL3SRCHOME > !TMP!\setgsdl3.bat
+call !TMP!\setgsdl3.bat
+del !TMP!\setgsdl3.bat
 
 :: set GSDLHOME to the 'web' subdirectory
-set GSDL3HOME=%GSDL3SRCHOME%\web
+set GSDL3HOME=!GSDL3SRCHOME!\web
 
 :: change if using external tomcat or ant
-set TOMCAT_HOME=%GSDL3SRCHOME%\packages\tomcat
-if exist "%GSDL3SRCHOME%\packages\ant\*.*" set ANT_HOME=%GSDL3SRCHOME%\packages\ant
+set TOMCAT_HOME=!GSDL3SRCHOME!\packages\tomcat
+if exist "!GSDL3SRCHOME!\packages\ant\*.*" set ANT_HOME=!GSDL3SRCHOME!\packages\ant
 
 :: other important environment variables
 set GSDLOS=windows
 
 :: ---- Set the CLASSPATH and PATH environment variables ----
-if "%GS_CP_SET%" == "yes" goto skipSetCp
-set CLASSPATH=%GSDL3HOME%\WEB-INF\classes;%GSDL3SRCHOME%\resources\java;%GSDL3SRCHOME%\cp.jar;%CLASSPATH%;
-set PATH=%PATH%;%GSDL3SRCHOME%\bin;%GSDL3SRCHOME%\bin\script;%GSDL3SRCHOME%\lib\jni;%ANT_HOME%\bin
+if "!GS_CP_SET!" == "yes" goto skipSetCp
+set CLASSPATH=!GSDL3HOME!\WEB-INF\classes;!GSDL3SRCHOME!\resources\java;!GSDL3SRCHOME!\cp.jar;!CLASSPATH!;
+set PATH=!PATH!;!GSDL3SRCHOME!\bin;!GSDL3SRCHOME!\bin\script;!GSDL3SRCHOME!\lib\jni;!ANT_HOME!\bin
 
 :: Override Imagemagick and Ghostscript paths to the bundled applications shipped with greenstone if they exists otherwise use default environment variables.
-if exist "%GSDL3SRCHOME%\gs2build\bin\windows\ghostscript\bin\gsdll32.dll" set GS_DLL=%GSDL3SRCHOME%\gs2build\bin\windows\ghostscript\bin\gsdll32.dll
-if exist "%GSDL3SRCHOME%\gs2build\bin\windows\ghostscript\lib\*.*" set GS_LIB=%GSDL3SRCHOME%\gs2build\bin\windows\ghostscript\lib
-if exist "%GSDL3SRCHOME%\gs2build\bin\windows\ghostscript\bin\*.*" set PATH=%GSDL3SRCHOME%\gs2build\bin\windows\ghostscript\bin;%PATH%
-if exist "%GSDL3SRCHOME%\gs2build\bin\windows\imagemagick\*.*" set PATH=%GSDL3SRCHOME%\gs2build\bin\windows\imagemagick;%PATH%
+if exist "!GSDL3SRCHOME!\gs2build\bin\windows\ghostscript\bin\gsdll32.dll" set GS_DLL=!GSDL3SRCHOME!\gs2build\bin\windows\ghostscript\bin\gsdll32.dll
+if exist "!GSDL3SRCHOME!\gs2build\bin\windows\ghostscript\lib\*.*" set GS_LIB=!GSDL3SRCHOME!\gs2build\bin\windows\ghostscript\lib
+if exist "!GSDL3SRCHOME!\gs2build\bin\windows\ghostscript\bin\*.*" set PATH=!GSDL3SRCHOME!\gs2build\bin\windows\ghostscript\bin;!PATH!
+if exist "!GSDL3SRCHOME!\gs2build\bin\windows\imagemagick\*.*" set PATH=!GSDL3SRCHOME!\gs2build\bin\windows\imagemagick;!PATH!
 
 :: a little dynamic set cp stuff
-if exist %TMP%\setcp.bat del %TMP%\setcp.bat
-for %%j in ("%TOMCAT_HOME%"\common\endorsed\*.jar) do echo set CLASSPATH=%%CLASSPATH%%;%%j>> %TMP%\setcp.bat
-for %%j in ("%TOMCAT_HOME%"\lib\*.jar) do echo set CLASSPATH=%%CLASSPATH%%;%%j>> %TMP%\setcp.bat
-if exist %TMP%\setcp.bat call %TMP%\setcp.bat
-if exist %TMP%\setcp.bat del %TMP%\setcp.bat
+if exist !TMP!\setcp.bat del !TMP!\setcp.bat
+for %%j in (!TOMCAT_HOME!\common\endorsed\*.jar) do echo set CLASSPATH=%%CLASSPATH%%;%%j>> !TMP!\setcp.bat
+for %%j in (!TOMCAT_HOME!\lib\*.jar) do echo set CLASSPATH=%%CLASSPATH%%;%%j>> !TMP!\setcp.bat
+if exist !TMP!\setcp.bat call !TMP!\setcp.bat
+if exist !TMP!\setcp.bat del !TMP!\setcp.bat
 
 set GS_CP_SET=yes
 :skipSetCp
@@ -82,16 +84,14 @@ if exist local\gs3-setup.bat (
 
 :: ---- Search for java ----
 set JAVA_MIN_VERSION=1.5.0_00
-set HINT=%CD%\packages\jre
+set HINT=!CD!\packages\jre
 ::if search4j is present, use it
 set FOUNDJAVAHOME=
 set RUNJAVA=
 if exist bin\search4j.exe (
-  for /F "tokens=*" %%r in ('bin\search4j.exe -d -p "%HINT%" -m %JAVA_MIN_VERSION%') do set FOUNDJAVAHOME=%%r
-  for /F "tokens=*" %%r in ('bin\search4j.exe -r -p "%HINT%" -m %JAVA_MIN_VERSION%') do set FOUNDJREHOME=%%r
+  for /F "tokens=*" %%r in ('bin\search4j.exe -d -p "!HINT!" -m !JAVA_MIN_VERSION!') do set FOUNDJAVAHOME=%%r
+  for /F "tokens=*" %%r in ('bin\search4j.exe -r -p "!HINT!" -m !JAVA_MIN_VERSION!') do set FOUNDJREHOME=%%r
 )
-
-setlocal enabledelayedexpansion
 
 if DEFINED FOUNDJAVAHOME  (
   set JAVA_HOME=!FOUNDJAVAHOME!
@@ -107,27 +107,27 @@ if DEFINED FOUNDJREHOME (
   goto summaryThenEnd
 )
 
-if exist "%HINT%\bin\java.exe" (
-  set JAVA_HOME=%HINT%
-  set PATH=%JAVA_HOME%\bin;%PATH%
-  set RUNJAVA=%JAVA_HOME%\bin\java.exe
+if exist "!HINT!\bin\java.exe" (
+  set JAVA_HOME=!HINT!
+  set PATH=!JAVA_HOME!\bin;!PATH!
+  set RUNJAVA=!JAVA_HOME!\bin\java.exe
   goto summaryThenEnd
 )
 
-if exist "%JAVA_HOME%\bin\java.exe" (
-  set PATH=%JAVA_HOME%\bin;%PATH%
-  set RUNJAVA=%JAVA_HOME%\bin\java.exe
-  echo Using java at %JAVA_HOME%
+if exist "!JAVA_HOME!\bin\java.exe" (
+  set PATH=!JAVA_HOME!\bin;!PATH!
+  set RUNJAVA=!JAVA_HOME!\bin\java.exe
+  echo Using java at !JAVA_HOME!
   echo WARNING: Greenstone has not checked the version number of this java installation
   echo          The source distribution of Greenstone3 requires java 1.5 or greater
   echo          SVN users may still use java 1.4
   goto summaryThenEnd
 )
 
-if exist "%JRE_HOME%\bin\java.exe" (
-  set PATH=%JRE_HOME%\bin;%PATH%
-  set RUNJAVA=%JRE_HOME%\bin\java.exe
-  echo Using java at %JRE_HOME%
+if exist "!JRE_HOME!\bin\java.exe" (
+  set PATH=!JRE_HOME!\bin;!PATH!
+  set RUNJAVA=!JRE_HOME!\bin\java.exe
+  echo Using java at !JRE_HOME!
   echo WARNING: Greenstone has not checked the version number of this java installation
   echo          The source distribution of Greenstone3 requires java 1.5 or greater
   echo          SVN users may still use java 1.4
@@ -140,9 +140,9 @@ goto end
 
 :summaryThenEnd
 
-echo GSDL3SRCHOME : %GSDL3SRCHOME%
-echo GSDL3HOME    : %GSDL3HOME%
-echo JAVA         : %RUNJAVA%
+echo GSDL3SRCHOME : !GSDL3SRCHOME!
+echo GSDL3HOME    : !GSDL3HOME!
+echo JAVA         : !RUNJAVA!
 if "!ANT_HOME!" == "" (
    echo.
    echo ANT_HOME is not yet set.
@@ -154,7 +154,10 @@ if "!ANT_HOME!" == "" (
 )
 echo.
 
-endlocal
+:: End localisation of variables that started with the set local/set enabledelayedexpansion command
+:: Restore global variables that would otherwise be lost at script's end due to their having been initialised in a 
+:: set local/set enabledelayedexpansion section. See http://ss64.com/nt/endlocal.html
+endlocal & set RUNJAVA=%RUNJAVA%
 
 set PATH=%PATH%
 set GSDL3HOME=%GSDL3HOME%
@@ -162,5 +165,6 @@ set GSDL3SRCHOME=%GSDL3SRCHOME%
 set JAVA_HOME=%JAVA_HOME%
 set JRE_HOME=%JRE_HOME%
 set ANT_HOME=%ANT_HOME%
+set RUNJAVA=%RUNJAVA%
 
 :end
