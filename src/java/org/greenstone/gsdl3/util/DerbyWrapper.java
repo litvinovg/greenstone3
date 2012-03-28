3eb6ca84b637a911d9f3c6e18cc88276799ff6de
ak19 <ak19@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2012-03-28 02:45:03 +0000

    1. Updated txt2usersDB.java to compile again and to also work with usersDB2txt.java such that the output of running one becomes the exact input required by the other and vice-versa. So running usersDB2txt and then txt2usersDB on the txt file generated by the former, will generate a DB file that can be fed back into usersDB2txt which will once more produce an identical text output. 2. Created the ChangePwdUsersDB.java file which build.xml's new ant config-admin target calls to reset the admin password.
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@25308 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- src/java/org/greenstone/gsdl3/util/DerbyWrapper.java
+++ src/java/org/greenstone/gsdl3/util/DerbyWrapper.java
@@ -344,6 +344,8 @@ public class DerbyWrapper
 		}
 	}
 
+    // findUser(null) will return all users, which is why a UserQueryResult 
+    // (a vector of UserTermInfo) is returned
 	public UserQueryResult findUser(String username) throws SQLException
 	{
 		UserQueryResult userQueryResult = new UserQueryResult();
@@ -463,9 +465,20 @@ public class DerbyWrapper
 
 	public void db2txt()
 	{
+	    System.err.println(db2txtString());		
+	}
+
+	public String db2txtString()
+	{
+	    //String db2txt = "Error in converting db2txt string.";
+	    String db2txt = "";
 		try
 		{
-			conn.setAutoCommit(false);
+		    conn.setAutoCommit(false); // An exception at this line can happen when the GS3 tomcat server is already running
+		                               // and GS3 is already accessing the usersDB when this function independently tries to
+		                               // connect to it (via usersDB2txt.java's main(). For an explanation of the possible 
+		                               // reasons, see http://db.apache.org/derby/papers/DerbyTut/embedded_intro.html
+		                               // section "Embedded Derby supports multiple users in one JVM".
 			String sql_list_all_user = "select username, password, accountstatus, comment, email from " + USERS;
 			ResultSet rs = state.executeQuery(sql_list_all_user);
 
@@ -496,15 +509,16 @@ public class DerbyWrapper
 					returnedGroups += groupsSet.getString("role");
 				}
 				conn.commit();
-
-				System.err.println("-------------------------------------");
-				System.err.println("USERNAME = " + user.get("username"));
-				System.err.println("PASSWORD = " + user.get("password"));
-				System.err.println("GROUPS = " + returnedGroups);
-				System.err.println("STATUS = " + user.get("status"));
-				System.err.println("COMMENT = " + user.get("comment"));
-				System.err.println("EMAIL = " + user.get("email"));
-				System.err.println("-------------------------------------");
+				
+				StringBuffer buffer = new StringBuffer();//("-------------------------------------");
+				buffer.append("USERNAME = " + user.get("username"));
+				buffer.append("\nPASSWORD = " + user.get("password"));
+				buffer.append("\nGROUPS = " + returnedGroups);
+				buffer.append("\nSTATUS = " + user.get("status"));
+				buffer.append("\nCOMMENT = " + user.get("comment"));
+				buffer.append("\nEMAIL = " + user.get("email"));
+				buffer.append("\n-------------------------------------\n");
+				db2txt = buffer.toString();
 			}
 
 			conn.commit();
@@ -513,6 +527,8 @@ public class DerbyWrapper
 		catch (Exception ex)
 		{
 			ex.printStackTrace();
+		} finally {
+		    return db2txt;
 		}
 	}
 
--- src/java/org/greenstone/gsdl3/util/txt2usersDB.java
+++ src/java/org/greenstone/gsdl3/util/txt2usersDB.java
@@ -37,6 +37,7 @@ public class txt2usersDB {
 	    DerbyWrapper dw=new DerbyWrapper();
 	    dw.connectDatabase(args[1],false);
 	    boolean delete_rows = dw.deleteAllUser();
+	    dw.closeDatabase();
 	    if (!delete_rows){
 		System.out.println("Couldn't delete rows of the users table");
 		System.exit(0);
@@ -46,36 +47,74 @@ public class txt2usersDB {
 	    String groups=null;
 	    String accountstatus=null;
 	    String comment=null;
+	    String email=null;
 	    while ((str = in.readLine()) != null) {
 		//ystem.out.println(str);
-		if (str.startsWith("<")){
+		
+		if(str.indexOf(" = ") != -1) { // works with DerbyWrapper.db2txt() and usersDB2txt.java. Fields listed as: USERNAME = admin
+		    String field=str.substring(0,str.indexOf(" = "));
+		    if (field.equalsIgnoreCase("email")){
+			email=str.substring(str.indexOf(" = ")+3,str.length());
+		    }
+		    if (field.equalsIgnoreCase("comment")){
+			comment=str.substring(str.indexOf(" = ")+3,str.length());
+		    }
+		    if (field.equalsIgnoreCase("status")){
+			accountstatus=str.substring(str.indexOf(" = ")+3,str.length());
+		    }
+		    if (field.equalsIgnoreCase("groups")){
+			groups=str.substring(str.indexOf(" = ")+3,str.length());
+		    }
+		    if (field.equalsIgnoreCase("password")){
+			//password=dw.rot13(str.substring(str.indexOf(">")+1,str.length()));
+			password=str.substring(str.indexOf(" = ")+3,str.length());
+		    }
+		    if (field.equalsIgnoreCase("username")){
+			username=str.substring(str.indexOf(" = ")+3,str.length());
+		    }
+		}		
+		else if (str.startsWith("<")){ // fields listed as: <username>admin
 		    String field=str.substring(1,str.indexOf(">"));
+		    if (field.equals("email")){
+			email=str.substring(str.indexOf(">")+1,str.length());
+		    }
 		    if (field.equals("comment")){
 			comment=str.substring(str.indexOf(">")+1,str.length());
 		    }
-		    if (field.equals("enabled")){
+		    if (field.equals("enabled") || field.equals("status")){
 			accountstatus=str.substring(str.indexOf(">")+1,str.length());
 		    }
 		    if (field.equals("groups")){
 			groups=str.substring(str.indexOf(">")+1,str.length());
 		    }
 		    if (field.equals("password")){
-			password=dw.rot13(str.substring(str.indexOf(">")+1,str.length()));
+			//password=dw.rot13(str.substring(str.indexOf(">")+1,str.length()));
+			password=str.substring(str.indexOf(">")+1,str.length());
 		    }
 		    if (field.equals("username")){
 			username=str.substring(str.indexOf(">")+1,str.length());
 		    }
 		}
-		if (str.equals("----------------------------------------------------------------------")){
-		    if ((username!=null) && (password!=null) && (groups!=null) && (accountstatus!=null) && (comment!=null)){
+		else if (str.equals("----------------------------------------------------------------------")
+			 || str.equals("-------------------------------------")) {		    
+		    
+		    if ((username!=null) && (password!=null) && (groups!=null) && (accountstatus!=null) && (comment!=null) && (email!=null)) {
 			dw.connectDatabase(args[1],false);
-			dw.addUser(username, password, groups, accountstatus, comment);
+			dw.addUser(username, password, groups, accountstatus, comment, email);
 			username=null;
 			password=null;
 			groups=null;
 			accountstatus=null;
 			comment=null;
-			dw.connectDatabase(args[1],false);
+			email=null;
+			//dw.connectDatabase(args[1],false); // should this be closeDatabase()????
+			dw.closeDatabase();
+		    }
+		}
+		else { // encrypted passwords can span multiple lines for some reason
+		       // assume that is the case here
+		    if(password != null) { 
+			password = password + "\n" + str;
 		    }
 		}
 	    }	
