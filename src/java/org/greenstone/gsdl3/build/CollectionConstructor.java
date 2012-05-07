5fab26890981f5bd50b9085af24433f900f4894c
sjm84 <sjm84@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2012-05-07 21:49:00 +0000

    Reformatting this file ahead of some changes
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@25537 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- src/java/org/greenstone/gsdl3/build/CollectionConstructor.java
+++ src/java/org/greenstone/gsdl3/build/CollectionConstructor.java
@@ -8,97 +8,127 @@ import javax.swing.event.EventListenerList;
 import org.w3c.dom.Element;
 
 /** base class for collection construction */
-public abstract class CollectionConstructor 
-    extends Thread {
+public abstract class CollectionConstructor extends Thread
+{
 
-    /** the site in which building is to take place */
-    protected String site_home = null;
-    /** the name of the site*/
-    protected String site_name = null;
-    /** the name of the collection */
-    protected String collection_name = null;
-    /** the stage of construction */
-    protected int process_type = -1;
-    /** other arguments/parameters for the construction process - in a paramList */
-    protected Element process_params = null;
-    /** the list of listeners for the process */
-    protected EventListenerList listeners = null;
-    /** A flag used to determine if this process has been asked to cancel. */
-    protected boolean cancel = false;
-    
-    public CollectionConstructor(String name) {
-	super(name);
-	this.listeners = new EventListenerList();
-    }
-	
-    /** carry out any set up stuff - returns false if couldn't set up properly
-     */
-    public boolean configure() {
-	return true;
-    }
-    public void stopAction() {
-	this.cancel = true;
-    }
-    
-    public void setActionType(int type) {
-	this.process_type = type;
-    }
-    public void setSiteHome(String site_home) {
-	this.site_home = site_home;
-	
-	File siteHomeFile = new File(site_home);
-	this.site_name = siteHomeFile.getName();
-    }
-    public void setCollectionName(String coll_name) {
-	this.collection_name = coll_name;
-    }
-    public void setProcessParams(Element params) {
-	this.process_params = params;
-    }
-    public boolean addListener(ConstructionListener listener) {
-	this.listeners.add(ConstructionListener.class, listener);
-	return true;
-    }
-    public boolean removeListener(ConstructionListener listener) {
-	this.listeners.remove(ConstructionListener.class, listener);
-	return true;
-    }
+	/** the site in which building is to take place */
+	protected String site_home = null;
+	/** the name of the site */
+	protected String site_name = null;
+	/** the name of the collection */
+	protected String collection_name = null;
+	/** the stage of construction */
+	protected int process_type = -1;
+	/** other arguments/parameters for the construction process - in a paramList */
+	protected Element process_params = null;
+	/** the list of listeners for the process */
+	protected EventListenerList listeners = null;
+	/** A flag used to determine if this process has been asked to cancel. */
+	protected boolean cancel = false;
 
-    protected void sendProcessBegun(ConstructionEvent evt) {
-	Object[] concerned = this.listeners.getListenerList();
-	for(int i = 0; i < concerned.length ; i+=2) {
-	    if(concerned[i] == ConstructionListener.class) {
-		((ConstructionListener)concerned[i+1]).processBegun(evt);
-	    }
+	public CollectionConstructor(String name)
+	{
+		super(name);
+		this.listeners = new EventListenerList();
 	}
-    }
-    protected void sendProcessComplete(ConstructionEvent evt) {
-	Object[] concerned = this.listeners.getListenerList();
-	for(int i = 0; i < concerned.length ; i+=2) {
-	    if(concerned[i] == ConstructionListener.class) {
-		((ConstructionListener)concerned[i+1]).processComplete(evt);
-	    }
-	}
-    }
-    
-    protected void sendProcessStatus(ConstructionEvent evt) {
 
-	Object[] concerned = this.listeners.getListenerList();
-	for(int i = 0; i < concerned.length ; i+=2) {
-	    if(concerned[i] == ConstructionListener.class) {
-		((ConstructionListener)concerned[i+1]).processStatus(evt);
-	    }
+	/**
+	 * carry out any set up stuff - returns false if couldn't set up properly
+	 */
+	public boolean configure()
+	{
+		return true;
 	}
-    }
-    protected void sendMessage(ConstructionEvent evt) {
 
-	Object[] concerned = this.listeners.getListenerList();
-	for(int i = 0; i < concerned.length ; i+=2) {
-	    if(concerned[i] == ConstructionListener.class) {
-		((ConstructionListener)concerned[i+1]).message(evt);
-	    }
+	public void stopAction()
+	{
+		this.cancel = true;
 	}
-    }
-    
-    abstract public void run();
+
+	public void setActionType(int type)
+	{
+		this.process_type = type;
+	}
+
+	public void setSiteHome(String site_home)
+	{
+		this.site_home = site_home;
+
+		File siteHomeFile = new File(site_home);
+		this.site_name = siteHomeFile.getName();
+	}
+
+	public void setCollectionName(String coll_name)
+	{
+		this.collection_name = coll_name;
+	}
+
+	public void setProcessParams(Element params)
+	{
+		this.process_params = params;
+	}
+
+	public boolean addListener(ConstructionListener listener)
+	{
+		this.listeners.add(ConstructionListener.class, listener);
+		return true;
+	}
+
+	public boolean removeListener(ConstructionListener listener)
+	{
+		this.listeners.remove(ConstructionListener.class, listener);
+		return true;
+	}
+
+	protected void sendProcessBegun(ConstructionEvent evt)
+	{
+		Object[] concerned = this.listeners.getListenerList();
+		for (int i = 0; i < concerned.length; i += 2)
+		{
+			if (concerned[i] == ConstructionListener.class)
+			{
+				((ConstructionListener) concerned[i + 1]).processBegun(evt);
+			}
+		}
+	}
+
+	protected void sendProcessComplete(ConstructionEvent evt)
+	{
+		Object[] concerned = this.listeners.getListenerList();
+		for (int i = 0; i < concerned.length; i += 2)
+		{
+			if (concerned[i] == ConstructionListener.class)
+			{
+				((ConstructionListener) concerned[i + 1]).processComplete(evt);
+			}
+		}
+	}
+
+	protected void sendProcessStatus(ConstructionEvent evt)
+	{
+
+		Object[] concerned = this.listeners.getListenerList();
+		for (int i = 0; i < concerned.length; i += 2)
+		{
+			if (concerned[i] == ConstructionListener.class)
+			{
+				((ConstructionListener) concerned[i + 1]).processStatus(evt);
+			}
+		}
+	}
+
+	protected void sendMessage(ConstructionEvent evt)
+	{
+
+		Object[] concerned = this.listeners.getListenerList();
+		for (int i = 0; i < concerned.length; i += 2)
+		{
+			if (concerned[i] == ConstructionListener.class)
+			{
+				((ConstructionListener) concerned[i + 1]).message(evt);
+			}
+		}
+	}
+
+	abstract public void run();
 }
