116df0ab7136f50e253e1679e76a12088014c96e
sjm84 <sjm84@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2012-05-14 02:28:38 +0000

    Some tidying up as well as adding system commands
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@25591 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- src/java/org/greenstone/gsdl3/core/URLFilter.java
+++ src/java/org/greenstone/gsdl3/core/URLFilter.java
@@ -30,7 +30,6 @@ public class URLFilter implements Filter
 
 	//Restricted URLs
 	protected static final String SITECONFIG_URL = "sites/[^/]+/siteConfig.xml";
-
 	protected static final ArrayList<String> _restrictedURLs;
 	static
 	{
@@ -38,6 +37,21 @@ public class URLFilter implements Filter
 		restrictedURLs.add(SITECONFIG_URL);
 		_restrictedURLs = restrictedURLs;
 	}
+
+	//Constants
+	protected static final String DOCUMENT_PATH = "document";
+	protected static final String COLLECTION_PATH = "collection";
+	protected static final String PAGE_PATH = "page";
+	protected static final String SYSTEM_PATH = "system";
+
+	protected static final String METADATA_RETRIEVAL_SERVICE = "DocumentMetadataRetrieve";
+	protected static final String ASSOCIATED_FILE_PATH = "/index/assoc/";
+	protected static final String COLLECTION_FILE_PATH = "/collect/";
+	
+	protected static final String SYSTEM_SUBACTION_CONFIGURE = "configure";
+	protected static final String SYSTEM_SUBACTION_RECONFIGURE = "reconfigure";
+	protected static final String SYSTEM_SUBACTION_ACTIVATE = "activate";
+	protected static final String SYSTEM_SUBACTION_DEACTIVATE = "deactivate";
 
 	public void init(FilterConfig filterConfig) throws ServletException
 	{
@@ -64,10 +78,11 @@ public class URLFilter implements Filter
 				return;
 			}
 
-			if (url.contains("/index/assoc/"))
+			//If the user is trying to access a collection file we need to run a security check
+			if (url.contains(ASSOCIATED_FILE_PATH))
 			{
 				String dir = null;
-				int dirStart = url.indexOf("/index/assoc/") + "/index/assoc/".length();
+				int dirStart = url.indexOf(ASSOCIATED_FILE_PATH) + ASSOCIATED_FILE_PATH.length();
 				int dirEnd = -1;
 				if (dirStart < url.length() && url.indexOf("/", dirStart) != -1)
 				{
@@ -83,7 +98,7 @@ public class URLFilter implements Filter
 				}
 
 				String collection = null;
-				int colStart = url.indexOf("/collect/") + "/collect/".length();
+				int colStart = url.indexOf(COLLECTION_FILE_PATH) + COLLECTION_FILE_PATH.length();
 				int colEnd = -1;
 				if (colStart < url.length() && url.indexOf("/", colStart) != -1)
 				{
@@ -113,7 +128,7 @@ public class URLFilter implements Filter
 				}
 
 				Element metaMessage = gsDoc.createElement(GSXML.MESSAGE_ELEM);
-				Element metaRequest = GSXML.createBasicRequest(gsDoc, GSXML.REQUEST_TYPE_PROCESS, collection + "/DocumentMetadataRetrieve", new UserContext());
+				Element metaRequest = GSXML.createBasicRequest(gsDoc, GSXML.REQUEST_TYPE_PROCESS, collection + "/" + METADATA_RETRIEVAL_SERVICE, new UserContext());
 				metaMessage.appendChild(metaRequest);
 
 				Element paramList = gsDoc.createElement(GSXML.PARAM_ELEM + GSXML.LIST_MODIFIER);
@@ -178,8 +193,9 @@ public class URLFilter implements Filter
 			}
 			else
 			{
+				//If we have a jsessionid on the end of our URL we want to ignore it
 				int index;
-				if((index = url.indexOf(";jsessionid")) != -1)
+				if ((index = url.indexOf(";jsessionid")) != -1)
 				{
 					url = url.substring(0, index);
 				}
@@ -190,12 +206,12 @@ public class URLFilter implements Filter
 					String[] defaultParamValues = null;
 
 					//COLLECTION
-					if (segments[i].equals("collection") && (i + 1) < segments.length)
+					if (segments[i].equals(COLLECTION_PATH) && (i + 1) < segments.length)
 					{
 						gRequest.setParameter(GSParams.COLLECTION, segments[i + 1]);
 					}
 					//DOCUMENT
-					else if (segments[i].equals("document") && (i + 1) < segments.length)
+					else if (segments[i].equals(DOCUMENT_PATH) && (i + 1) < segments.length)
 					{
 						gRequest.setParameter(GSParams.DOCUMENT, segments[i + 1]);
 
@@ -203,12 +219,50 @@ public class URLFilter implements Filter
 						defaultParamValues = new String[] { "d", "hierarchy" };
 					}
 					//PAGE
-					else if (segments[i].equals("page") && (i + 1) < segments.length)
+					else if (segments[i].equals(PAGE_PATH) && (i + 1) < segments.length)
 					{
 						gRequest.setParameter(GSParams.SUBACTION, segments[i + 1]);
 
 						additionalParameters = new String[] { GSParams.ACTION };
 						defaultParamValues = new String[] { "p" };
+					}
+					//SYSTEM
+					else if (segments[i].equals(SYSTEM_PATH) && (i + 1) < segments.length)
+					{
+						String sa = segments[i + 1];
+						if (sa.equals(SYSTEM_SUBACTION_CONFIGURE) || sa.equals(SYSTEM_SUBACTION_RECONFIGURE))
+						{
+							sa = "c";
+						}
+						else if (sa.equals(SYSTEM_SUBACTION_ACTIVATE))
+						{
+							sa = "a";
+						}
+						else if (sa.equals(SYSTEM_SUBACTION_DEACTIVATE))
+						{
+							sa = "d";
+						}
+						
+						if (sa.equals("c") && (i + 2) < segments.length)
+						{
+							gRequest.setParameter(GSParams.SYSTEM_CLUSTER, segments[i + 2]);
+						}
+						
+						if (sa.equals("a") && (i + 2) < segments.length)
+						{
+							gRequest.setParameter(GSParams.SYSTEM_MODULE_TYPE, "collection");
+							gRequest.setParameter(GSParams.SYSTEM_MODULE_NAME, segments[i + 2]);
+						}
+						
+						if (sa.equals("d") && (i + 2) < segments.length)
+						{
+							gRequest.setParameter(GSParams.SYSTEM_CLUSTER, segments[i + 2]);
+						}
+
+						gRequest.setParameter(GSParams.SUBACTION, sa);
+
+						additionalParameters = new String[] { GSParams.ACTION };
+						defaultParamValues = new String[] { "s" };
 					}
 					//ADMIN
 					else if (segments[i].equals("admin") && (i + 1) < segments.length)
@@ -252,13 +306,12 @@ public class URLFilter implements Filter
 						{
 							serviceName = segments[i + 1];
 							gRequest.setParameter("s", serviceName);
-							
+
 							additionalParameters = new String[] { GSParams.ACTION, GSParams.SUBACTION, GSParams.REQUEST_TYPE };
 							defaultParamValues = new String[] { "q", "", "d" };
 						}
-						if((i + 2) < segments.length)
+						if ((i + 2) < segments.length)
 						{
-							System.err.println("HUH??");
 							if (serviceName.equals("TextQuery") || serviceName.equals("RawQuery"))
 							{
 								additionalParameters = new String[] { GSParams.ACTION, GSParams.SUBACTION, GSParams.REQUEST_TYPE, "s1.maxDocs", "s1.hitsPerPage", "s1.level", "s1.sortBy", "s1.index", "s1.startPage" };
