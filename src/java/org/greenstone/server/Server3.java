16b1f8109654d09c92a39cf67a94cbe9b2f99b04
sjm84 <sjm84@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2012-05-30 03:28:29 +0000

    Reformatting this file ahead of a chance
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@25716 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- src/java/org/greenstone/server/Server3.java
+++ src/java/org/greenstone/server/Server3.java
@@ -1,4 +1,3 @@
-
 package org.greenstone.server;
 
 import java.io.File;
@@ -11,56 +10,60 @@ import org.greenstone.util.GlobalProperties;
 
 public class Server3 extends BaseServer
 {
-    
-    public Server3(String gsdl3_src_home, String lang)
-    {
-	super(gsdl3_src_home,lang, gsdl3_src_home+File.separatorChar+"build.properties", "logs");
 
-	Property = new Server3Property();
+	public Server3(String gsdl3_src_home, String lang)
+	{
+		super(gsdl3_src_home, lang, gsdl3_src_home + File.separatorChar + "build.properties", "logs");
 
-	String frame_title = dictionary.get("ServerControl.Frame_Title");
-	server_control_ = new Server3Control(this,frame_title);
+		Property = new Server3Property();
 
-	/** Ant command tagets for managing Web server */
-	START_CMD     = "start";
-	RESTART_CMD   = "restart";
-	CONFIGURE_CMD = "configure";
-	STOP_CMD      = "stop";
+		String frame_title = dictionary.get("ServerControl.Frame_Title");
+		server_control_ = new Server3Control(this, frame_title);
 
-	autoStart();
-    }
-        
-    protected int runTarget(String cmd)
-    {
-	RunAnt runAnt = new RunAnt();
-	runAnt.setTargetCmd(cmd);
-	runAnt.run();
-   	return runAnt.getTargetState();
-    }
-    
-    public String getBrowserURL() {
-	return GlobalProperties.getFullGSDL3WebAddress()+ config_properties.getProperty(BaseServer.Property.DEFAULT_SERVLET);
-    }
+		/** Ant command tagets for managing Web server */
+		START_CMD = "start";
+		RESTART_CMD = "restart";
+		CONFIGURE_CMD = "configure";
+		STOP_CMD = "stop";
 
-    public void reload() {
-	GlobalProperties.reload(); // properties file may have changed, so reload it
-    }
-
-    public static void main (String[] args) 
-    {
-    	if ((args.length < 1) || (args.length>2)) {
-	    System.err.println("Usage: java org.greenstone.server.Server3 <gsdl3-src-home> [lang]");
-	    System.exit(1);
+		autoStart();
 	}
 
-	String gsdl3_src_home = args[0];	
-	File gsdl3_src_dir = new File(gsdl3_src_home);
-	if (!gsdl3_src_dir.isDirectory()) {
-	    System.err.println("src directory does not exist!");
-	    System.exit(1);
+	protected int runTarget(String cmd)
+	{
+		RunAnt runAnt = new RunAnt();
+		runAnt.setTargetCmd(cmd);
+		runAnt.run();
+		return runAnt.getTargetState();
 	}
 
-	String lang = (args.length==2) ? args[1] : "en";
-	new Server3(gsdl3_src_home,lang);
-    }
+	public String getBrowserURL()
+	{
+		return GlobalProperties.getFullGSDL3WebAddress() + config_properties.getProperty(BaseServer.Property.DEFAULT_SERVLET);
+	}
+
+	public void reload()
+	{
+		GlobalProperties.reload(); // properties file may have changed, so reload it
+	}
+
+	public static void main(String[] args)
+	{
+		if ((args.length < 1) || (args.length > 2))
+		{
+			System.err.println("Usage: java org.greenstone.server.Server3 <gsdl3-src-home> [lang]");
+			System.exit(1);
+		}
+
+		String gsdl3_src_home = args[0];
+		File gsdl3_src_dir = new File(gsdl3_src_home);
+		if (!gsdl3_src_dir.isDirectory())
+		{
+			System.err.println("src directory does not exist!");
+			System.exit(1);
+		}
+
+		String lang = (args.length == 2) ? args[1] : "en";
+		new Server3(gsdl3_src_home, lang);
+	}
 }
