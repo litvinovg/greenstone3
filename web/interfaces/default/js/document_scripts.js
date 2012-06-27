d0cc2d1a9f8d1ed87585d133ebe46b3c3f2a9520
kjdon <kjdon@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2012-06-27 21:45:04 +0000

    documentNodeContent template was renamed to documentNodeText
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@25840 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- web/interfaces/default/js/document_scripts.js
+++ web/interfaces/default/js/document_scripts.js
@@ -19,7 +19,7 @@ function getTextForSection(sectionID, callback)
 	template +=     '<xsl:for-each select="/page/pageResponse/document//documentNode[@nodeID = \'' + sectionID + '\']">';
 	template +=       '<xsl:call-template name="sectionImage"/>';
 	template +=       '<div id="text' + sectionID + '">';
-	template +=         '<xsl:call-template name="documentNodeContent"/>';
+	template +=         '<xsl:call-template name="documentNodeText"/>';
 	template +=       '</div>';
 	template +=     '</xsl:for-each>';
 	template +=   '</text>';
