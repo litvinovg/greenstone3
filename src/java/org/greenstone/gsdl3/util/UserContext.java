848077fe096e0fad5bd6bdef5a614700968db742
sjm84 <sjm84@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2013-03-13 01:15:33 +0000

    Added groups to the usercontext (and some minor reformatting)
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@27077 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- src/java/org/greenstone/gsdl3/util/UserContext.java
+++ src/java/org/greenstone/gsdl3/util/UserContext.java
@@ -6,46 +6,65 @@ public class UserContext
 {
 	protected String _userID = null;
 	protected String _lang = null;
-	
-	public UserContext(){}
-	
+	protected String[] _groups = null;
+
+	public UserContext()
+	{
+	}
+
 	public UserContext(Element xmlRequest)
 	{
 		_lang = xmlRequest.getAttribute(GSXML.LANG_ATT);
 		_userID = xmlRequest.getAttribute(GSXML.USER_ID_ATT);
+		_groups = xmlRequest.getAttribute(GSXML.GROUPS_ATT).split(",");
 	}
 
-	public UserContext(String lang, String userID)
+	public UserContext(String lang, String userID, String[] groups)
 	{
 		_lang = lang;
 		_userID = userID;
+		_groups = groups;
 	}
-	
+
 	public void setLanguage(String lang)
 	{
 		_lang = lang;
 	}
-	
+
 	public void setUserID(String userID)
 	{
 		_userID = userID;
 	}
-	
+
+	public void setGroups(String[] groups)
+	{
+		_groups = groups;
+	}
+
 	public String getLanguage()
 	{
-		if(_lang != null)
+		if (_lang != null)
 		{
 			return _lang;
 		}
 		return "";
 	}
-	
+
 	public String getUserID()
 	{
-		if(_userID != null)
+		if (_userID != null)
 		{
 			return _userID;
 		}
 		return "";
+	}
+
+	public String[] getGroups()
+	{
+		if (_groups != null)
+		{
+			return _groups;
+		}
+		return new String[0];
 	}
 }
