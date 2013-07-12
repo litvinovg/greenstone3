d76d4e7b91913077f7328d61ae6d9c0acc4490b7
davidb <davidb@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2013-07-12 11:01:12 +0000

    Additional work to help support Greenstone running off a read-only medium, such as a DVD
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@27833 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- gs3-server.bat
+++ gs3-server.bat
@@ -15,9 +15,30 @@ set GSDL3PATH=
 :: Setup Greenstone, unless it has already been done
 if "%GSDL3SRCHOME%" == "" call "%GSDL3PATH%\gs3-setup.bat" SetEnv > nul
 
+
+:: See if Greenstone3 web folder is writable
+  echo.
+  echo Checking if the Greenstone3 web directory is writable ...
+  (echo This is a temporary file. It is safe to delete it. > "!GSDL3HOME!\testing.tmp" ) 2>nul
+  if exist "%GSDL3HOME%\testing.tmp" goto isWritable
+
+:: Is read-only
+  set gsdl3_writablehome=%TMP%\greenstone\web
+  set opt_properties=-Dgsdl3home.isreadonly=true -Dgsdl3.writablehome=%gsdl3_writablehome%
+  echo ... no.
+  echo Setting Greenstone3 web home writable area to be: %gsdl3_writablehome%
+  goto runJava
+
+:isWritable
+  del "%GSDL3HOME%\testing.tmp"
+  set opt_properties=
+  echo  ... yes.
+
+:runJava
+
 set GSDL3PATH=
 
-"%RUNJAVA%" -cp "%CLASSPATH%" org.greenstone.server.Server3 "%GSDL3SRCHOME%"
+"%RUNJAVA%" -cp "%CLASSPATH%" %opt_properties% org.greenstone.server.Server3 "%GSDL3SRCHOME%"
 
 
 
