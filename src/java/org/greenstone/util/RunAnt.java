924f37dbd771efaeefa0c1ca568331becea4206a
davidb <davidb@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2013-07-12 05:10:14 +0000

    Class extended to include support for storing properties that would be defined when an 'ant' task is run.  Introduced to help support Greenstone running off a read-only medium, such as a DVD
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@27826 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- src/java/org/greenstone/util/RunAnt.java
+++ src/java/org/greenstone/util/RunAnt.java
@@ -4,22 +4,34 @@ import org.greenstone.util.RunTarget;
 
 public class RunAnt extends RunTarget 
 {
-    public RunAnt()
+    String opt_cmd_args = "";
+
+    public RunAnt(String opt_args)
     {
 	super();
+
+	if (opt_args!=null) {
+	    opt_cmd_args = opt_args + " ";
+	}
 
 	targetSuccess  = "BUILD SUCCESSFUL";
 	targetFailed   = "BUILD FAILED";
 	targetFinished = "Total time";
     }
+
+    public RunAnt()
+    {
+	this(null);
+    }
+
     
     public void setTargetCmd(String target) 
     {
 	String osName = System.getProperty("os.name");
       	if (osName.startsWith("Windows")) {
-	    targetCmd = "ant.bat " + target;
+	    targetCmd = "ant.bat " + opt_cmd_args + target;
 	} else {
-	    targetCmd = "ant " + target;
+	    targetCmd = "ant " + opt_cmd_args + target;
 	}
     }
 }
