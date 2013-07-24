c6e79d364fa8dbb707589c36a6e6f11883255d41
sjm84 <sjm84@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2013-07-24 00:48:17 +0000

    Part of moving the userDB from localsite to web
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@27924 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- src/java/org/greenstone/gsdl3/util/ModifyUsersDB.java
+++ src/java/org/greenstone/gsdl3/util/ModifyUsersDB.java
@@ -23,7 +23,7 @@ import org.greenstone.gsdl3.service.Authentication;
 //import org.greenstone.admin.guiext.PropertiesStep;
 
 // To run this from the command-line, first make sure that the tomcat server is stopped, then run:
-// java -cp /full/path/to/GS3/web/WEB-INF/lib/gsdl3.jar:/full/path/to/GS3/web/WEB-INF/lib/derby.jar org.greenstone.gsdl3.util.usersDB2txt web/sites/localsite/etc/usersDB/
+// java -cp /full/path/to/GS3/web/WEB-INF/lib/gsdl3.jar:/full/path/to/GS3/web/WEB-INF/lib/derby.jar org.greenstone.gsdl3.util.usersDB2txt web/etc/usersDB/
 public class ModifyUsersDB {
     
     public static void main(String[] args) throws SQLException {
