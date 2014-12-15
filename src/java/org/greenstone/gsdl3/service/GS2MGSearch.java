litvinovg/greenstone3

commit ee86ea542fe3f38c7a8042d6a53b6967fccc480d

setting case and stem based on param defaults
git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@29630 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

src/java/org/greenstone/gsdl3/service/GS2MGSearch.java
@@ -70,9 +70,9 @@ extends AbstractGS2TextSearch {
         if (!super.configure (info, extra_info)){
             return false;
         }
-        this.mg_src.setMaxNumeric (this.maxnumeric);
 	// internally mg uses 50, so set this here
 	paramDefaults.put(MAXDOCS_PARAM, "50");
+        this.mg_src.setMaxNumeric (this.maxnumeric);
         return true;
     }
     
@@ -219,9 +219,10 @@ extends AbstractGS2TextSearch {
     protected boolean setStandardQueryParams(HashMap<String, Serializable> params)
     {
 	// set the default settings that gs uses
+      this.mg_src.setMaxDocs(paramDefaults.get(MAXDOCS_PARAM));
 	this.mg_src.setReturnTerms(true);
-	this.mg_src.setCase(true);
-	this.mg_src.setStem(false);	
+	this.mg_src.setCase(paramDefaults.get(CASE_PARAM).equals(BOOLEAN_PARAM_ON) ? true : false);
+	this.mg_src.setStem(paramDefaults.get(STEM_PARAM).equals(BOOLEAN_PARAM_ON) ? true : false);	
 	Set entries = params.entrySet();
 	Iterator i = entries.iterator();
 	while (i.hasNext()) {

commit 628b22ad3a158f9162d0197917fce96c547349a7

Convert MAXDOCS_PARAM to int when calling setMaxDocs. Fixes error introduced in r29630. Now it compiles again.
git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@29637 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

src/java/org/greenstone/gsdl3/service/GS2MGSearch.java
@@ -219,7 +219,7 @@ extends AbstractGS2TextSearch {
     protected boolean setStandardQueryParams(HashMap<String, Serializable> params)
     {
 	// set the default settings that gs uses
-      this.mg_src.setMaxDocs(paramDefaults.get(MAXDOCS_PARAM));
+      this.mg_src.setMaxDocs(Integer.parseInt(paramDefaults.get(MAXDOCS_PARAM)));
 	this.mg_src.setReturnTerms(true);
 	this.mg_src.setCase(paramDefaults.get(CASE_PARAM).equals(BOOLEAN_PARAM_ON) ? true : false);
 	this.mg_src.setStem(paramDefaults.get(STEM_PARAM).equals(BOOLEAN_PARAM_ON) ? true : false);	