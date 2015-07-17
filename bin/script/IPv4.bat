b0d5037277aaea5b81665056a7b62ea415ef4344
ak19 <ak19@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2015-07-17 05:45:54 +0000

    Modified IPv4 scripts to take the parameter -format-for-tomcat-context if it needs to format the IPv4 address. If not passed this flag, it will return the IPv4 address unformatted. build.xml is now updated to pass this flag to the scripts.
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@30038 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- bin/script/IPv4.bat
+++ bin/script/IPv4.bat
@@ -1,7 +1,12 @@
 @echo off
+
+rem This script will echo the IPv4 of this windows machine. E.g. 100.200.300.45
+rem If passed -format-for-tomcat-context, it will echo the same with a pipe
+rem symbol up front and all the . escaped with a "\". E.g. "|100\.200\.300\.45"
+
 for /f "usebackq tokens=2 delims=:" %%a in (`ipconfig ^| find "IPv4"`) do (set result=%%a)
 
-:: To lop off extra space in front of IPv4 number, end with:
+:: To lop off the single extra space in front of IPv4 number, end with:
 ::do (set result=%a& echo %result:~1%)
 :: Alternative: for /f "tokens=2 delims=:" %a in ('ipconfig ^| findstr /C:"IPv4 Address"') do echo %a
 :: still need to remove space up front for that too.
@@ -10,6 +15,12 @@ for /f "usebackq tokens=2 delims=:" %%a in (`ipconfig ^| find "IPv4"`) do (set r
 set result=%result:~1%
 
 :: The above produces the IPv4 number, e.g. 100.200.300.45
+
+
+rem Check if we're requested to format the IPv4 for the solr.xml tomcat context file
+if [%1]==[] goto done
+if not "%1"=="-format-for-tomcat-context" goto done
+
 :: Still need to replace . with \.
 :: http://scripts.dragon-it.co.uk/scripts.nsf/docs/batch-search-replace-substitute!OpenDocument&ExpandSection=3&BaseTarget=East&AutoFramed
 :: Syntax: %string:SEARCH=REPLACE%
@@ -24,5 +35,12 @@ set result=%result: =%
 ::if not "%result%" == "" 
 ::set result=^|%result%
 
-:: Echo a vertical bar up front, regardless of whether the result is empty
 echo ^|%result%
+goto fin
+
+
+:done
+:: Echo a vertical bar up front, regardless of whether the result is empty
+echo %result%
+
+:fin
--- bin/script/IPv4.sh
+++ bin/script/IPv4.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script will echo the IPv4 of this unix machine. E.g. 100.200.300.45
+# If passed -format-for-tomcat-context, it will echo the same with a pipe
+# symbol up front and all the . escaped with a "\". E.g. "|100\.200\.300\.45"
+
 #http://www.wikihow.com/Find-Your-IP-Address-on-a-Mac#Finding_Your_Internal_IP_Using_the_Terminal_sub
 #http://stackoverflow.com/questions/1469849/how-to-split-one-string-into-multiple-strings-in-bash-shell
 #http://www.linuxforums.org/forum/red-hat-fedora-linux/193076-ifconfig-doesnt-display-ipv4-address.html
@@ -10,26 +14,30 @@
 
 #echo `ifconfig | grep "inet " | grep -v 127.0.0.1`|cut -d' ' -f 2|cut -d':' -f 2
 
-
 tmp=`ifconfig | grep "inet " | grep -v 127.0.0.1`
 
 tmp=`echo $tmp|cut -d' ' -f 2|cut -d':' -f 2`
 
-#http://stackoverflow.com/questions/13210880/replace-one-substring-for-another-string-in-shell-script
-#${original_string//searchterm/$string_to_replace_searchterm_with}
-replace="."
-replacement="\."
+# if requested to format the IPv4 for the solr.xml tomcat context file
+if [ "$1" == "-format-for-tomcat-context" ]; then
 
-tmp=${tmp//$replace/$replacement}
-
-# next, if the tmp variable is not the empty string, prefix the | operator
-# http://unix.stackexchange.com/questions/146942/how-can-i-test-if-a-variable-is-empty-or-contains-only-spaces
-# Can just return | if $tmp is empty, as that doesn't really matter. Will be consistent with windows
-if [[ ! -z "${tmp// }" ]]; then
+    #http://stackoverflow.com/questions/13210880/replace-one-substring-for-another-string-in-shell-script
+    #${original_string//searchterm/$string_to_replace_searchterm_with}
+    replace="."
+    replacement="\."
+    
+    tmp=${tmp//$replace/$replacement}
+    
+    # next, if the tmp variable is not the empty string, prefix the | operator
+    # http://unix.stackexchange.com/questions/146942/how-can-i-test-if-a-variable-is-empty-or-contains-only-spaces
+    # Can just return | if $tmp is empty, as that doesn't really matter. Will be consistent with windows
+    if [[ ! -z "${tmp// }" ]]; then
 	tmp="|$tmp"
-else
+    else
 	tmp="|"
+    fi
+
 fi
 
-
 echo $tmp
+
