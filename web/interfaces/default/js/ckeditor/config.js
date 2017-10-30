667b719ce56dfa3f0939d9798de6b4e12f489b44
ak19 <ak19@a4f36ff5-3332-0410-bbfd-b1ff9fae65ff>
2017-10-30 05:41:26 +0000

    There was a problem about attributes like align=justify going missing from <p> html elements in doc.xml after a user edits a document. I had been thinking it was an XSLT issue, but Kathy found that the CKEditor was the culprit. Reading up, it seems the CKEditor has an Advanced Content Filter (ACF) mode, which is set to automatic by default and which deletes disallowed elements and other things. I've now adjusted the CKEditor config to to turn off the automatic ACF, so it will the HTML elelements in doc.xml as is if the user didn't make the edit.
    git-svn-id: http://svn.greenstone.org/main/trunk/greenstone3@32057 a4f36ff5-3332-0410-bbfd-b1ff9fae65ff

--- web/interfaces/default/js/ckeditor/config.js
+++ web/interfaces/default/js/ckeditor/config.js
@@ -9,4 +9,14 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+    // Stop CKEditor from removing the attributes from HTML elements in doc.xml
+    // e.g. <P ALIGN="JUSTIFY"></P> in the original doc.xml gets turned into
+    // <p></p> when viewing the doc in editing mode and then, this ends up
+    // getting saved with user changes upon SaveAndRebuild.
+    // http://nightly.ckeditor.com/17-10-29-07-04/full/samples/old/datafiltering.html
+    // https://sdk.ckeditor.com/samples/acf.html
+    // https://stackoverflow.com/questions/15659390/ckeditor-automatically-strips-classes-from-div
+    // The following stops CKEDITOR from meddling with existing HTML:
+    config.allowedContent = true;
 };
